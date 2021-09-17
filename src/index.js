c31038dee7f0e2a4f5e470745850c5cc4dea58b5
Dinislam Quantori <qdin@MacBook-Pro-Dinislam-2.local>
2021-09-17 14:47:32 +0300

    add webpack config; \nadd bootstrap;

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.scss';
+
+const rssForm = document.getElementById('rss-form');
+rssForm.addEventListener('submit', (event) => {
+    console.log('Submit');
+    event.preventDefault();
+});
