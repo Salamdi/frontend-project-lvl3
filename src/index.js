c96dd3d821cf844b88d7f38ec86aad0d84ea294f
Salamdi <dinislam91@gmail.com>
2022-01-01 22:59:21 +0300

    validate urls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import './index.scss';
+import * as yup from 'yup';
+
+const submitRss = (rss) => Promise.resolve(rss);
 
 const rssForm = document.getElementById('rss-form');
+const errorMessageElement = document.getElementById('error-message');
+
+const schema = yup.array()
+  .of(
+    yup
+      .string()
+      .url('Link must be a valid URL'),
+  )
+  .test({
+    test: (urls) => {
+      const [lastUrl, ...previousUrls] = urls.reverse();
+      return !previousUrls.includes(lastUrl);
+    },
+    name: 'unique',
+    message: 'RSS already exists',
+  });
+
+const rssUrls = [];
+
 rssForm.addEventListener('submit', (event) => {
-  console.log('Submit');
+  const formData = new FormData(event.target);
+  const url = formData.get('url');
+  schema
+    .validate(rssUrls)
+    .then(() => {
+      if (!errorMessageElement.classList.contains('d-none')) {
+        errorMessageElement.classList.add('d-none');
+      }
+      submitRss(url);
+    })
+    .then(rssUrls.push(url))
+    .catch((error) => {
+      errorMessageElement.textContent = error;
+      errorMessageElement.classList.remove('d-none');
+    });
   event.preventDefault();
 });
