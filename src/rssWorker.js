a312b3390b5e76149bd9477ca8821ee3ae9aabb2
Salamdi <dinilam91@gmail.com>
2022-05-10 23:32:57 +0600

    fix worker logic

--- src/rssWorker.js
+++ src/rssWorker.js
@@ -7,18 +7,20 @@ export default () => {
   const WORKER_TIMEOUT = 5000;
 
   const runWorker = () => {
-    state.rssUrls.forEach((url) => {
-      http.get('', { params: { url } })
-        .then((response) => response.data.contents)
-        .then(parseXML)
-        .then(({ posts }) => differenceBy(posts, state.posts, 'id'))
-        .then((freshPosts) => {
-          state.posts = state.posts.concat(freshPosts);
-        })
-        .catch(console.error);
-    });
 
-    setTimeout(runWorker, WORKER_TIMEOUT);
+    setTimeout(() => {
+      state.rssUrls.forEach((url) => {
+        http.get('', { params: { url } })
+          .then((response) => response.data.contents)
+          .then(parseXML)
+          .then(({ posts }) => differenceBy(posts, state.posts, 'id'))
+          .then((freshPosts) => {
+            state.posts = state.posts.concat(freshPosts);
+          })
+          .catch(console.error);
+      });
+      runWorker();
+    }, WORKER_TIMEOUT);
   };
 
   return (path, _, previousValue) => {
