47c78ad6dd7fea49e04bd9381b0ac7e8f6d055c6
Salamdi <dinilam91@gmail.com>
2022-05-25 17:29:03 +0600

    fix worker

--- src/rssWorker.js
+++ src/rssWorker.js
@@ -7,16 +7,15 @@ const WORKER_TIMEOUT = 5000;
 export default (state) => {
   const runWorker = () => {
     setTimeout(() => {
-      state.rssUrls.forEach((url) => {
-        http.get('', { params: { url } })
-          .then((response) => {
-            const { posts } = parseXML(response.data.contents);
-            const freshPosts = differenceBy(posts, state.posts);
-            state.posts = state.posts.concat(freshPosts);
-          })
-          .catch(console.error);
-      });
-      runWorker();
+      const promises = state.rssUrls.map((url) => http.get('', { params: { url } })
+        .then((response) => {
+          const { posts } = parseXML(response.data.contents);
+          const freshPosts = differenceBy(posts, state.posts);
+          state.posts = state.posts.concat(freshPosts);
+        })
+        .catch(console.error));
+      Promise.all(promises)
+        .finally(runWorker);
     }, WORKER_TIMEOUT);
   };
 
